000a333019e8881096783c56d008617aba5ac28b
Mohsen Mokhtari Sarab <m.mokhtari2012@gmail.com>
2019-03-07 00:33:50 +0300

    JS and CSS Clock

---
+++ app.js
@@ -0,0 +1,26 @@
+const secondHand = document.querySelector('.second-hand');
+const minsHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setDate() {
+  const now = new Date();
+
+  //For the seconds hand
+  const seconds = now.getSeconds();
+  const secondsDegrees = ((seconds / 60) * 360) + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+  //For the minutes hand
+  const mins = now.getMinutes();
+  const minsDegrees = ((mins / 60) * 360) + ((seconds / 60) * 6) + 90;
+  minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+  //For the hours hand
+  const hour = now.getHours();
+  const hourDegrees = ((hour / 12) * 360) + ((mins / 60) * 30) + 90;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+}
+
+//The time interval for one second is 1000 milliseconds
+setInterval(setDate, 1000);
+setDate();
